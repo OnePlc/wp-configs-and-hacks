4a8ba4afe5d08dabb9dc03486bdadb53a9de42e1
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2019-09-30 08:03:57 +0200

    Remove COOKIE_DOMAIN
    COOKIE_DOMAIN causes login issues on child pages. seems to work fine now

--- wp-configs/multisite.multidomain.wp-config.php
+++ wp-configs/multisite.multidomain.wp-config.php
@@ -25,7 +25,6 @@ define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
 define( 'WP_ALLOW_MULTISITE', true );
-define( 'COOKIE_DOMAIN', 'mytoplevel.domain' ); // only root domain
 define( 'COOKIEPATH',  $_SERVER['HTTP_HOST'] . '/' ); // You should set this explicitely.
 define( 'SITECOOKIEPATH', $_SERVER['HTTP_HOST'] . '/' ); // You should set this explicitely.
 define( 'ADMIN_COOKIE_PATH', SITECOOKIEPATH . 'wp-admin' );
